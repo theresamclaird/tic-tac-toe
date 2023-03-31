f07a635755a0b6f11e276c214fd4ff847c5878b6
theresamclaird <theresa@mclairdfamily.com>
2023-03-31 09:54:19 -0700

    announces winner

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -5,7 +5,7 @@ import "./styles.css";
 
 const Navbar = () => {
     const [showConfiguration, setShowConfiguration] = useState(false);
-    const { nextPlayer } = useContext(GameContext);
+    const { nextPlayer, winner } = useContext(GameContext);
     const handleConfigurationButtonClick = () => setShowConfiguration(!showConfiguration);
 
     return (
@@ -13,7 +13,7 @@ const Navbar = () => {
             <header className="navbar">
                 <button onClick={handleConfigurationButtonClick} className="configuration-button"><span role="img" aria-label="configuration button">⚙️</span></button>
                 <h1 className="title">Tic Tac Toe</h1>
-                <p className="next-player">Next Player: {nextPlayer}</p>
+                <p className="announcement">{winner ? `Winner: ${winner}` : `Next Player: ${nextPlayer}`}</p>
             </header>
             {showConfiguration && <Configuration close={() => setShowConfiguration(false)} />}
         </>
